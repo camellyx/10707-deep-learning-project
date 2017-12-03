671949476684a06384491b12fa211ef9e1500dc4
Michael Kuchnik <michaelkuchnik@gmail.com>
2017-12-03 17:23:49 -0500

    Add mutliagent naive q learning

--- multiagent-particle-envs/dqn_tag.py
+++ multiagent-particle-envs/dqn_tag.py
@@ -8,6 +8,7 @@ import os
 import pickle
 from collections import namedtuple
 import code
+from dqn import DQN
 
 from make_env import make_env
 
@@ -16,7 +17,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', default='simple_tag', type=str)
     parser.add_argument('--learning_rate', default=5e-5, type=float)
-    parser.add_argument('--train_episodes', default=1e3, type=int)
+    parser.add_argument('--train_episodes', default=1e5, type=int)
     parser.add_argument('--render', default=False,
                         action="store_true")
     parser.add_argument('--benchmark', default=False,
@@ -24,17 +25,27 @@ def main():
     options = parser.parse_args()
 
     env = make_env(options.env, options.benchmark)
+    print("action space", env.action_space)
+    print("observation space", env.observation_space)
+    print("env.n", env.n)
+    print("env.world.dim_c", env.world.dim_c)
+    dqns = [DQN(env.action_space[agent_i].n, env.observation_space[agent_i].shape[0]) for agent_i in range(env.n)]
     state = env.reset()
     for step in itertools.count():
+        t = (step + 1) * 0.005
         if step >= options.train_episodes:
             break
         if options.render:
             env.render()
-        action = []
-        for i in range(env.n):
-            # See policy.py: InteractivePolicy.action()
-            action.append(np.random.random(4 + env.world.dim_c))
-        next_state, reward, done, info = env.step(action)
+        agent_actions = []
+        for agent_i in range(env.n):
+            # Calculate agent policy
+            a = dqns[agent_i].choose_action(state[agent_i], t)
+            onehot_action = np.zeros(4 + env.world.dim_c)
+            onehot_action[a] = 1
+            agent_actions.append(onehot_action)
+        #print("agent_actions", agent_actions)
+        state, reward, done, info = env.step(agent_actions)
         if any(done):
             env.render()
             break
