a6b88a1b46515e1d4d65373b037a0f5bdf6c56f9
Yixin Luo <camellyx@gmail.com>
2017-12-06 12:00:42 -0500

    rfind problem

--- multiagent-particle-envs/general_utilities.py
+++ multiagent-particle-envs/general_utilities.py
@@ -4,15 +4,14 @@ import os
 import pathlib
 import json
 
+
 def ensure_directory_exists(base_directory):
     """
     Makes a directory if it does not exist
     """
-    try:
+    if not os.path.exists(base_directory):
         os.makedirs(base_directory)
-    except OSError as ex:
-        if ex.errno != errno.EEXIST:
-            raise ex
+
 
 def load_dqn_weights_if_exist(dqns, weights_filename_prefix, weights_filename_extension=".h5"):
     """
@@ -20,10 +19,12 @@ def load_dqn_weights_if_exist(dqns, weights_filename_prefix, weights_filename_ex
     """
     for i, dqn in enumerate(dqns):
         # TODO should not work if only some weights available?
-        dqn_filename = weights_filename_prefix + str(i) + weights_filename_extension
+        dqn_filename = weights_filename_prefix + \
+            str(i) + weights_filename_extension
         if os.path.isfile(dqn_filename):
             print("Found old weights to use for agent {}".format(i))
             dqn.load(dqn_filename)
+
 
 def save_dqn_weights(dqns, weights_filename_prefix, weights_filename_extension=".h5"):
     """
@@ -34,8 +35,10 @@ def save_dqn_weights(dqns, weights_filename_prefix, weights_filename_extension="
         dump_dirs = pathlib.Path(*p.parts[:-1])
         ensure_directory_exists(dump_dirs)
     for i, dqn in enumerate(dqns):
-        dqn_filename = weights_filename_prefix + str(i) + weights_filename_extension
+        dqn_filename = weights_filename_prefix + \
+            str(i) + weights_filename_extension
         dqn.save(dqn_filename)
+
 
 def dump_dict_as_json(dict_to_dump, filename):
     p = pathlib.Path(filename)
@@ -45,6 +48,7 @@ def dump_dict_as_json(dict_to_dump, filename):
     json_str = json.dumps(dict_to_dump)
     with open(filename, "w") as jsonfile:
         jsonfile.write(json_str)
+
 
 class Time_Series_Statistics_Store(object):
     """
@@ -56,13 +60,16 @@ class Time_Series_Statistics_Store(object):
         1     | 0.5
         2     | 0.3
     """
+
     def __init__(self, header):
         self.statistics = []
         self.header = header
+
     def add_statistics(self, data):
         if len(data) != len(self.header):
             raise ValueError("Data length does not match header")
         self.statistics.append(data)
+
     def dump(self, dump_filename="statistics.csv"):
         p = pathlib.Path(dump_filename)
         if len(p.parts) > 1:
