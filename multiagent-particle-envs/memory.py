6210848ed21e0136992919cf3ece09ceefae6b5f
Michael Kuchnik <michaelkuchnik@gmail.com>
2017-12-09 03:23:43 -0500

    Add some comments and string printing to memory

--- multiagent-particle-envs/memory.py
+++ multiagent-particle-envs/memory.py
@@ -15,6 +15,10 @@ class Memory:
             self.pointer += 1
 
     def sample(self, batch, agents=1):
+        """
+        If 1 agent, assumes no data about other agents.
+        If 2+ agents, assumes data contains all agent data.
+        """
         if agents == 1:
             states = np.array([self.data[i][0] for i in batch])
             actions = np.array([self.data[i][1] for i in batch])
@@ -33,3 +37,15 @@ class Memory:
         dones = np.array([self.data[i][4] for i in batch])
 
         return states, actions, rewards, states_next, dones
+
+    def __str__(self):
+        memory_state = ""
+        for s, a, r, sn, done in self.data:
+            if isinstance(s, list):
+                # probably agents 2+
+                for i in s:
+                    memory_state += "{},".format(i.shape)
+                memory_state += ";"
+            else:
+                memory_state += "{};".format(s.shape)
+        return memory_state
